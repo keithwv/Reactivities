3d004ba001adababe83facc6239848d4d3a91513
keith vickery <keithwv@hotmail.com>
2024-08-17 20:42:01 -0600

    End of Section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,22 +1,29 @@
 import { Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityLIst";
 
-import ActivityForm from "../form/ActivityForm";
-import ActivityDetails from "../details/activityDetails";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { useEffect } from "react";
 
 export default observer(function ActivityDashboard() {
   const { activityStore } = useStore();
-  const { selectedActivity, editMode } = activityStore;
+  const { loadActivities, activityRegistry } = activityStore;
+
+  useEffect(() => {
+    if (activityRegistry.size <= 1) loadActivities();
+  }, [activityRegistry.size, loadActivities]);
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading app..." />;
+
   return (
     <Grid>
       <Grid.Column width="10">
         <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
-        {selectedActivity && !editMode && <ActivityDetails />}
-        {editMode && <ActivityForm />}
+        <h2>Activity filters</h2>
       </Grid.Column>
     </Grid>
   );
--- client-app/src/features/activities/details/activityDetails.tsx
+++ client-app/src/features/activities/details/activityDetails.tsx
@@ -1,12 +1,24 @@
 import { Button, Card, CardContent, Image } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { observer } from "mobx-react-lite";
+import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
 
-export default function ActivityDetails() {
+export default observer(function ActivityDetails() {
   const { activityStore } = useStore();
-  const { selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+  const {
+    selectedActivity: activity,
+    loadActivity,
+    loadingInitial,
+  } = activityStore;
+  const { id } = useParams();
 
-  if (!activity) return <LoadingComponent />;
+  useEffect(() => {
+    if (id) loadActivity(id);
+  }, [id, loadActivity]);
+
+  if (loadingInitial || !activity) return <LoadingComponent />;
 
   return (
     <Card fluid>
@@ -21,19 +33,15 @@ export default function ActivityDetails() {
       <CardContent extra>
         <Button.Group width="2">
           <Button
-            onClick={() => openForm(activity.id)}
+            as={Link}
+            to={`/manage/${activity.id}`}
             basic
             color="blue"
             content="Edit"
           />
-          <Button
-            onClick={cancelSelectedActivity}
-            basic
-            color="grey"
-            content="Cancel"
-          />
+          <Button as={Link} to='/activities' basic color="grey" content="Cancel" />
         </Button.Group>
       </CardContent>
     </Card>
   );
-}
+});
