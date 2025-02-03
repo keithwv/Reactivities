478153bc383f49f23b5aaa13e00c29c0702493b0
Keith Vickery <keithwv@hotmail.com>
2025-02-02 17:34:55 -0700

    END OF SECTION 12

---
+++ API/DTOs/RegisterDto,.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required] [EmailAddress] public required string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=/*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
+        public required string Password { get; set; }
+
+        [Required] public required string DisplayName { get; set; }
+        public required string Username { get; set; }
+    }
+}
--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -4,18 +4,15 @@ using Application.Core;
 
 namespace API.Middleware
 {
-    public class ExceptionMiddleware
+    public class ExceptionMiddleware(
+        RequestDelegate next,
+        ILogger<ExceptionMiddleware> logger,
+        IHostEnvironment env)
     {
-        private readonly RequestDelegate _next;
-        private readonly ILogger<ExceptionMiddleware> _logger;
-        private readonly IHostEnvironment _env;
-        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
-            IHostEnvironment env)
-        {
-            _env = env;
-            _logger = logger;
-            _next = next;
-        }
+        private readonly RequestDelegate _next = next;
+        private readonly ILogger<ExceptionMiddleware> _logger = logger;
+        private readonly IHostEnvironment _env = env;
+
         public async Task InvokeAsync(HttpContext context)
         {
             try
@@ -28,9 +25,9 @@ namespace API.Middleware
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                 var response = _env.IsDevelopment()
-                    ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString()!)
                     : new AppException(context.Response.StatusCode, "Internal Server Error");
-                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
                 var json = JsonSerializer.Serialize(response, options);
                 await context.Response.WriteAsync(json);
             }
